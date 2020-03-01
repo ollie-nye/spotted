5613a42e5a30e8ce5da3636eec81119713648ccb
Ollie Nye <ollie@nyemail.co.uk>
2020-03-01 13:04:00 +0000

    Import all personalities in class definition

--- src/spotted_personality.py
+++ src/spotted_personality.py
@@ -1,12 +1,26 @@
-import cv2 as cv
-
+import json
 from spotted_attribute import SpottedAttribute
 
 class SpottedPersonality:
   def __init__(self, json):
+    self.id = json['id']
     self.manufacturer = json['manufacturer']
     self.model = json['model']
+    self.channels = json['channels']
     
     self.attributes = []
     for attribute_json in json['attributes']:
       self.attributes.append(SpottedAttribute(attribute_json))
+
+  def get_attribute(self, name):
+    for attribute in self.attributes:
+      if attribute.name == name:
+        return attribute
+    return None
+
+personalities_json = json.load(open('config/personalities.json'))
+
+personalities = []
+
+for pers in personalities_json:
+  personalities.append(SpottedPersonality(pers))
