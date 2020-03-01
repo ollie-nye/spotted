7f1ad1cf96f2bc1207d7e03e1a8607f8c35dbff6
Ollie Nye <ollie@nyemail.co.uk>
2020-03-01 13:03:22 +0000

    Make camera class responsible for frame processing in separate thread

--- src/spotted_camera.py
+++ src/spotted_camera.py
@@ -1,5 +1,25 @@
 import cv2 as cv
 import numpy as np
+import math
+from datetime import datetime
+
+from spotted_coordinate import SpottedCoordinate
+
+def scale(value, old_min, old_max, new_min, new_max):
+  old_range = (old_max - old_min)
+  new_range = (new_max - new_min)
+  return (((value - old_min) * new_range) / old_range) + new_min
+
+def imfill(frame):
+  im_flood_fill = frame.copy()
+  h, w = frame.shape[:2]
+  mask = np.zeros((h + 2, w + 2), np.uint8)
+  im_flood_fill = im_flood_fill.astype('uint8')
+  cv.floodFill(im_flood_fill, mask, (0, 0), 255)
+  im_flood_fill_inv = cv.bitwise_not(im_flood_fill)
+  # im_flood_fill_inv = im_flood_fill_inv.astype('float')
+  frame = frame.astype('uint8')
+  return frame | im_flood_fill_inv
 
 class SpottedCamera:
   def __init__(self, json):
@@ -23,59 +43,198 @@ class SpottedCamera:
     }
 
     self.resolution = {
-      'vertical': 1080,
-      'horizontal': 1920
+      'vertical': json['resolution']['vertical'],
+      'horizontal': json['resolution']['horizontal'],
     }
 
     self.capture = cv.VideoCapture(self.url)
 
-    self.background_frames = []
-    self.background_frame_length = 30.0
-    self.background_frames_populated = False
+    self.background_frame_length = 90.0
     self.current_background = None
-    self.current_background_frame = 0
+    self.current_frame = None
 
-  def average_background(self):
-    # self.current_background = [sum(pixel)/len(pixel) for pixel in zip(*self.background_frames)]
-    # self.current_background = np.mean([i for i in self.background_frames if i is not None], axis=0)
-    self.current_background = np.mean(self.background_frames, axis=0)
+    self.points_of_interest = []
 
-    # cv.accumulateWeighted()
-
-
-    # avg = np.array(self.background_frames[0], dtype=np.float)
-    # for frame in self.background_frames:
-    #   avg = cv.accumulate(frame, avg)
-    # self.current_background = avg
-
-    # avg_frame = self.background_frames[0]
-
-    # for row in range(self.resolution['vertical']):
-    #   for col in range(self.resolution['horizontal']):
-    #     pixel_sum = 0
-    #     for frame in self.background_frames:
-    #       pixel_sum += frame[row, col]
-    #     avg_frame[row, col] = pixel_sum / len(self.background_frames)
-    
-    # self.current_background = avg_frame
-
+  def y_offset(self, distance):
+    return math.tan(self.rotation['z']) * distance
 
   def update_background(self, new_frame):
     if self.current_background is None:
       self.current_background = np.array(new_frame, dtype=np.float)
     else:
       cv.accumulateWeighted(new_frame, self.current_background, 1.0 / self.background_frame_length)
-      # cv.accumulate(new_frame, self.current_background)
-    # if self.background_frames_populated == False:
-    #   self.background_frames.append(new_frame)
-    # else:
-    #   self.background_frames[self.current_background_frame] = new_frame
-    # self.average_background()
+
+  def begin_capture(self, calibration):
+    count = 0
+    start = datetime.now()
+    kernel = np.ones((8, 8),np.uint8)
+    while(1):
+      # print('Processing frame', count, ' with framerate', (count / (datetime.now() - start).total_seconds()))
+      count += 1
+      ret, frame = self.capture.read()
+      if frame is not None:
+        frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        frame = calibration.restore(frame)
+        
+        
+        # cv.threshold(frame, 120, 255, cv.THRESH_TOZERO, frame)
+        self.update_background(frame)
+
+        # frame[500:540, :] = 255
+
+        # cv.imshow('VIDEO', frame)
+
+        diff = np.subtract(frame, self.current_background)
+
+        # print(diff[0,0])
+
+        # ret, diff = cv.threshold(diff, 80, 255, cv.THRESH_TOZERO)
+        # ret, diff = cv.threshold(diff, 180, 255, cv.THRESH_TOZERO_INV)
+        ret, diff = cv.threshold(diff, 30, 255, cv.THRESH_BINARY)
+        # cv.normalize(diff, diff, 0, 255, cv.NORM_MINMAX)
 
 
-    # self.current_background_frame = (self.current_background_frame + 1) % self.background_frame_length
-    # if self.current_background_frame == 0:
-    #   self.background_frames_populated = True
+        # diff = float_to_int(diff)
+        # ret, diff = cv.threshold(float_to_int(diff), 200, 255, cv.THRESH_TOZERO)
+        # cv.imshow('VIDEO', float_to_int(camera.current_background))
 
-    # cv.imshow('VIDEO', np.array(self.current_background, dtype=np.uint8))
-    # cv.waitKey(1)
+        # diff = float_to_int(camera.current_background)
+
+        # self.current_frame = imfill(diff)
+
+        # self.current_frame = cv.morphologyEx(diff, cv.MORPH_CLOSE, kernel)
+        frame = cv.morphologyEx(diff, cv.MORPH_OPEN, kernel)
+
+        frame = np.array(frame, dtype=np.uint8)
+        # print(type(frame))
+        contours, hierarchy = cv.findContours(frame, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+
+        largest_contour = None
+        largest_area = 0
+        for contour in contours:
+          area = cv.contourArea(contour)
+          if area > largest_area:
+            largest_area = area
+            largest_contour = contour
+        
+        if largest_contour is not None:
+          M = cv.moments(largest_contour)
+          cX = int(M["m10"] / M["m00"])
+          cY = int(M["m01"] / M["m00"])
+          # draw the contour and center of the shape on the image
+
+          cv.normalize(frame, frame, 0, 50, cv.NORM_MINMAX)
+
+          cv.drawContours(frame, [largest_contour], -1, 150, 2)
+          cv.circle(frame, (cX, cY), 20, 255, -1)
+
+          horiz_midpoint = self.viewing_angle['horizontal'] / 2
+          vert_midpoint = self.viewing_angle['vertical'] / 2
+
+          angle_horizontal = scale(cX, 0, self.resolution['horizontal'], 0, self.viewing_angle['horizontal'])
+          angle_vertical = scale(cY, 0, self.resolution['vertical'], 0, self.viewing_angle['vertical'])
+
+          horiz_from_camera = (self.rotation['y'] - (self.viewing_angle['horizontal'] / 2) + angle_horizontal) % 360
+          vert_from_camera = (self.rotation['z'] - (self.viewing_angle['vertical'] / 2) + angle_vertical) % 360
+
+          # print((cX, cY), ' gave absolute angle ', horiz_from_camera, ', ', vert_from_camera)
+
+          # tan angle * 2m gives difference in real space from cameras location
+
+          horizontal_direction = 1
+          vertical_direction = 1
+
+          absolute_horiz = angle_horizontal
+          if absolute_horiz > horiz_midpoint:
+            absolute_horiz -= horiz_midpoint
+          else:
+            absolute_horiz = horiz_midpoint - absolute_horiz
+            horizontal_direction = -1
+
+          absolute_vert = angle_vertical
+          if absolute_vert > vert_midpoint:
+            absolute_vert -= vert_midpoint
+          else:
+            absolute_vert = vert_midpoint - absolute_vert
+            vertical_direction = -1
+
+          horiz_distance = math.tan(math.radians(absolute_horiz)) * 2
+          vert_distance = math.tan(math.radians(absolute_vert)) * 2
+
+          point = SpottedCoordinate(
+            #TODO: Fix these static values
+            4 - (self.position['x'] + (horiz_distance * horizontal_direction)),
+            self.position['y'] + self.y_offset(2) + (vert_distance * vertical_direction),
+            2
+          )
+
+          self.points_of_interest = [point]
+        else:
+          self.points_of_interest = []
+          # print(self.points_of_interest)
+          
+          # x_position = math.tan(math.radians(vert_from_camera)) * self.position['y']
+          # print(x_position)
+
+          # ox = self.position['x']
+          # oz = 0
+          # px = x_position
+          # pz = 0
+
+          # angle = math.radians(horiz_from_camera)
+
+          # qx = ox + math.cos(angle) * (px - ox) - math.sin(angle) * (pz - oz)
+          # qz = oz + math.sin(angle) * (px - ox) + math.cos(angle) * (pz - oz)
+          # print('x:', qx, 'y: 0 z:', qz)
+
+
+
+          # self.points_of_interest = [(cX, cY)]
+          # print(self.points_of_interest)
+
+        self.current_frame = frame
+        # self.current_frame = cv.dilate(diff, kernel)
+
+        # diff = cv.resize(diff, ( 1024, 768 ))
+        # # cv.imshow('VIDEO', float_to_int(camera.current_background))
+        # cv.imshow('VIDEO', diff/255)
+        # # cv.imshow('VIDEO', frame)
+        # cv.waitKey(1)
+
+  def process_frame(self, room):
+    # Cameras must be flat in the horizontal plane, no x rotation
+    # pois = [
+    #   { 'x': 0, 'y': 0 },
+    #   { 'x': 1920, 'y': 1080 },
+    #   { 'x': 1920, 'y': 0 },
+    #   { 'x': 0, 'y': 1080 },
+    #   { 'x': 960, 'y': 540 }
+    # ]
+
+    # midpoint_vertical = self.resolution['vertical'] / 2
+    # midpoint_horizontal = self.resolution['horizontal'] / 2
+
+    print(self.position)
+
+    for poi in self.points_of_interest:
+      angle_horizontal = scale(poi['x'], 0, self.resolution['horizontal'], 0, self.viewing_angle['horizontal'])
+      angle_vertical = scale(poi['y'], 0, self.resolution['vertical'], 0, self.viewing_angle['vertical'])
+
+      horiz_from_camera = (self.rotation['y'] - (self.viewing_angle['horizontal'] / 2) + angle_horizontal) % 360
+      vert_from_camera = (self.rotation['z'] - (self.viewing_angle['vertical'] / 2) + angle_vertical) % 360
+
+      print(poi, ' gave absolute angle ', horiz_from_camera, ', ', vert_from_camera)
+      
+      x_position = math.tan(math.radians(vert_from_camera)) * self.position['y']
+      print(x_position)
+
+      ox = self.position['x']
+      oz = 0
+      px = x_position
+      pz = 0
+
+      angle = math.radians(horiz_from_camera)
+
+      qx = ox + math.cos(angle) * (px - ox) - math.sin(angle) * (pz - oz)
+      qz = oz + math.sin(angle) * (px - ox) + math.cos(angle) * (pz - oz)
+      print('x:', qx, 'y: 0 z:', qz)
