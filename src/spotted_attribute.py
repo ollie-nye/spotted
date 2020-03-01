fdc3af1a6c90f2f1718767ba69815a919eff1b44
Ollie Nye <ollie@nyemail.co.uk>
2020-03-01 13:02:57 +0000

    Set default output values on each attribute

--- src/spotted_attribute.py
+++ src/spotted_attribute.py
@@ -3,7 +3,24 @@ class SpottedAttribute:
     self.name = json['name']
     self.offset = json['offset']
     self.attribute_type = json['attribute_type']
-    self.range = json['range']
-    self.default = json['default']
-    self.multiplier_type = json['multiplier_type']
-    self.multiplier_length = json['multiplier_length']
+
+    self.range = 255
+    if 'range' in json:
+      self.range = json['range']
+    
+    self.default = 255
+    if 'default' in json:
+      self.default = json['default']
+  
+    self.multiplier_type = 'normal'
+    if 'multiplier_type' in json:
+      self.multiplier_type = json['multiplier_type']
+    
+    self.multiplier_length = 1
+    if 'multiplier_length' in json:
+      self.multiplier_length = json['multiplier_length']
+
+    self.invert = False
+    if 'invert' in json:
+      if json['invert'] == "true":
+        self.invert = True
