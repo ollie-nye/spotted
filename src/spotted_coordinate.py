55f9e8cd9e7fea6afe908d2f6a503cd37b683864
Ollie Nye <ollie@nyemail.co.uk>
2020-03-03 01:03:09 +0000

    Add displace_by and as_vector methods

--- src/spotted_coordinate.py
+++ src/spotted_coordinate.py
@@ -1,4 +1,5 @@
 import math
+import numpy as np
 
 def pythagoras(ax, ay, bx, by):
   return math.sqrt(math.pow((ax - bx), 2) + math.pow((ay - by), 2))
@@ -21,3 +22,9 @@ class SpottedCoordinate:
     diff = self.diff(b)
     angle = math.atan2(diff.y, pythagoras(self.x, self.y, b.x, b.y))
     return math.degrees(angle)
+
+  def displace_by(self, position):
+    return SpottedCoordinate(self.x + position.x, self.y + position.y, self.z + position.z)
+
+  def as_vector(self):
+    return np.array([self.x, self.y, self.z])
