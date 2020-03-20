cb83aec7b192af286d40594328f2d776fe6aaf33
Ollie Nye <ollie@nyemail.co.uk>
2020-03-20 12:01:02 +0000

    Fix broken sphericalcoordinate test after instance variable rename

--- tests/spotted/spherical_coordinate_test.py
+++ tests/spotted/spherical_coordinate_test.py
@@ -2,14 +2,13 @@ from spotted.spherical_coordinate import SphericalCoordinate
 from spotted.coordinate import Coordinate
 
 def test_init_r(spherical_coordinate):
-  assert spherical_coordinate.r == 0.1
-  assert spherical_coordinate.a == 0.2
-  assert spherical_coordinate.i == 0.3
+  assert spherical_coordinate.radius == 0.1
+  assert spherical_coordinate.azimuth == 0.2
+  assert spherical_coordinate.inclination == 0.3
 
 def test_from_cartesian():
-  coord = Coordinate(1, 1, 1)
-  spherical = SphericalCoordinate.from_cartesian(coord)
+  spherical = SphericalCoordinate.from_cartesian(1, 1, 1)
 
-  assert spherical.r == 1.7320508075688772
-  assert spherical.a == 0.7853981633974483
-  assert spherical.i == 0.9553166181245093
+  assert spherical.radius == 1.7320508075688772
+  assert spherical.azimuth == 0.7853981633974483
+  assert spherical.inclination == 0.9553166181245093
