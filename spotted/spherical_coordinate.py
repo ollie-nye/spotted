852c945064402ad56c3c41405bdac95877cdef20
Ollie Nye <ollie@nyemail.co.uk>
2020-03-20 01:46:37 +0000

    Document SphericalCoordinate class

--- spotted/spherical_coordinate.py
+++ spotted/spherical_coordinate.py
@@ -1,21 +1,49 @@
-import math
-import numpy as np
+"""
+Spherical coordinate representation
+"""
 
-def pythagoras(ax, ay, bx, by):
-  return math.sqrt(math.pow((ax - bx), 2) + math.pow((ay - by), 2))
+import math
 
 class SphericalCoordinate:
-  def __init__(self, r, a, i):
-    self.radius = r
-    self.azimuth = a
-    self.inclination = i
-    self.elevation = (math.pi / 2) - i
-    # self.elevation = i
+  """
+  Spherical coordinate representation
+  """
 
+  def __init__(self, radius, azimuth, inclination):
+    """
+    Creates a SphericalCoordinate from the given parameters
+
+    Arguments:
+      radius {float} -- radius component
+      azimuth {float} -- azimuth component
+      inclination {float} -- inclination component
+
+    Returns:
+      SphericalCoordinate -- instance configured with given parameters
+    """
+
+    self.radius = radius
+    self.azimuth = azimuth
+    self.inclination = inclination
+    self.elevation = (math.pi / 2) - inclination
+
+  # pylint: disable=invalid-name
   @staticmethod
-# def from_cartesian(x, y, z):
-  def from_cartesian(y, z, x):
-    r = math.sqrt(x**2 + y**2 + z**2)
-    a = math.atan2(y, x)
-    i = math.atan2(math.sqrt(x**2 + y**2), z)
-    return SphericalCoordinate(r, a, i)
+  def from_cartesian(x, y, z):
+    """
+    Takes a cartesian coordinate in standard axis, converts it to spherical
+    0deg azimuth is +ve z axis
+
+    Arguments:
+      x {float} - x component
+      y {float} - y component
+      z {float} - z component
+
+    Returns:
+      SphericalCoordinate -- Pointing at the same position, from world origin
+    """
+
+    radius = math.sqrt(x**2 + y**2 + z**2)
+    azimuth = math.atan2(x, z)
+    inclination = math.atan2(math.sqrt(x**2 + z**2), y)
+    return SphericalCoordinate(radius, azimuth, inclination)
