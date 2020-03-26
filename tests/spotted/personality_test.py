ace176e00a9981c1afdbe3b9a5bf87df3c5ea619
Ollie Nye <ollie@nyemail.co.uk>
2020-03-26 03:41:50 +0000

    Fix broken tests

--- tests/spotted/personality_test.py
+++ tests/spotted/personality_test.py
@@ -1,29 +1,28 @@
-from spotted.personality import Personality, find_personality_by_id
+from spotted.personality import find_personality_by_id
 from spotted.attribute import Attribute
 
 def test_init(personality):
   assert personality.personality_id == 1
   assert personality.manufacturer == 'Hex'
   assert personality.model == '150w beam'
-  assert personality.channels == 16
-  assert len(personality.attributes) == 4
+  assert len(personality.modes) == 1
 
-def test_get_attribute(personality):
-  attribute = Attribute({'name': 'frog', 'offset': 8, 'attribute_type': 'position'})
-  personality.attributes.append(attribute)
+# def test_get_attribute(personality):
+#   attribute = Attribute({'name': 'frog', 'offset': 8, 'attribute_type': 'position'})
+#   personality.attributes.append(attribute)
 
-  attr = personality.get_attribute('frog')
-  assert attr == attribute
+#   attr = personality.get_attribute('frog')
+#   assert attr == attribute
 
-def test_get_attribute_not_exist(personality):
-  assert personality.get_attribute('notfrog') is None
+# def test_get_attribute_not_exist(personality):
+#   assert personality.get_attribute('notfrog') is None
 
 def test_find_personality_by_id(personality):
-  pers = find_personality_by_id(personality.personality_id)
+  pers = find_personality_by_id(personality.personality_id, 1)
 
-  assert pers.personality_id == personality.personality_id
+  assert pers.channels == 16
 
 def test_find_personality_by_id_not_exist():
-  pers = find_personality_by_id(100)
+  pers = find_personality_by_id(100, 1)
 
   assert pers is None
