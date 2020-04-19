7b08bad7f7bf45bd11d9836cb55aba83d3e706d6
Ollie Nye <ollie@nyemail.co.uk>
2020-04-19 10:53:16 +0100

    Add errors for incorrect network setup

--- spotted/error.py
+++ spotted/error.py
@@ -14,6 +14,8 @@ class ErrorCode(enum.Enum):
   MissingKey = 3
   EmptyKey = 4
   CameraConnect = 5
+  MissingInterface = 6
+  InterfaceAddress = 7
 
 def exit_with_error(error_code, details=None):
   """
@@ -25,14 +27,19 @@ def exit_with_error(error_code, details=None):
     details {object} -- Additional details to print in the error message
   """
 
-  if error_code == ErrorCode.MissingConfig:
+  if error_code is ErrorCode.MissingConfig:
     print('A required config file could not be found:', details)
-  elif error_code == ErrorCode.MissingKey:
+  elif error_code is ErrorCode.MissingKey:
     print(f"'{details}' key does not exist in the config")
-  elif error_code == ErrorCode.EmptyKey:
+  elif error_code is ErrorCode.EmptyKey:
     print(f"'{details}' key must contain at least one item")
-  elif error_code == ErrorCode.CameraConnect:
+  elif error_code is ErrorCode.CameraConnect:
     print(f"Camera '{details}' could not be opened.")
+  elif error_code is ErrorCode.MissingInterface:
+    print(f"Configured interface '{details[0]}' was not found on this host.")
+    print(f"Available interfaces are: {', '.join(details[1])}.")
+  elif error_code is ErrorCode.InterfaceAddress:
+    print(f"Configured interface '{details}' does not have an IPv4 address")
 
   print('Exiting')
   sys.exit(error_code)
