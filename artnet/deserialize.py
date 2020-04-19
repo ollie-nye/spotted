5619912d97f86ff3300765eb8028f0cf9e82379a
Ollie Nye <ollie@nyemail.co.uk>
2020-04-19 15:27:27 +0100

    Don't crash if the incoming artnet packet has a duff opcode

--- artnet/deserialize.py
+++ artnet/deserialize.py
@@ -12,13 +12,15 @@ def identify_header(incoming):
 
   if incoming[0:8].decode('ascii') == 'Art-Net\0': # This is an art-net packet
     opcode = incoming[8] + (incoming[9] << 8)
-    opcode = Opcode(opcode)
-    packet_data = incoming[12:]
-    if opcode is Opcode.OpPoll:
-      packet = Poll.deserialize(packet_data)
-    elif opcode is Opcode.OpPollReply:
-      #TODO: deserialize ArtPollReply
-      return True, opcode, None
-    return True, opcode, packet
-  else:
-    return False, opcode, None
+    try:
+      opcode = Opcode(opcode)
+      packet_data = incoming[12:]
+      if opcode is Opcode.OpPoll:
+        packet = Poll.deserialize(packet_data)
+      elif opcode is Opcode.OpPollReply:
+        #TODO: deserialize ArtPollReply
+        return True, opcode, None
+      return True, opcode, packet
+    except ValueError:
+      print('Unknown Art-Net packet received. Opcode', opcode)
+  return False, opcode, None
