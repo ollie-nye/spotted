f613d97129b0b2dd33b250beede488bde8e2078b
Ollie Nye <ollie@nyemail.co.uk>
2020-04-19 10:53:01 +0100

    Create system variables config

---
+++ config/system.py
@@ -0,0 +1,14 @@
+"""
+System configuration variables
+"""
+
+class SystemConfig:
+  """
+  System configuration variables
+  """
+
+  # Art-Net
+
+  oem_code = 0xffff # unassigned code
+  esta_code = 0x7fff # prototype code
+  transmit_rate = 'continuous' # or 'reduced'
