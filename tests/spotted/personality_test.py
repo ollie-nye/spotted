19c949a9a37f34c69d61a8e4c9862c5c09a59f1d
Ollie Nye <ollie@nyemail.co.uk>
2020-04-27 11:35:30 +0100

    Update personality tests

--- tests/spotted/personality_test.py
+++ tests/spotted/personality_test.py
@@ -1,21 +1,10 @@
 from spotted.personality import find_personality_by_id
-from spotted.attribute import Attribute
 
 def test_init(personality):
   assert personality.personality_id == 0
   assert personality.manufacturer == 'hex'
   assert personality.model == '150w beam'
   assert len(personality.modes) == 1
-
-# def test_get_attribute(personality):
-#   attribute = Attribute({'name': 'frog', 'offset': 8, 'attribute_type': 'position'})
-#   personality.attributes.append(attribute)
-
-#   attr = personality.get_attribute('frog')
-#   assert attr == attribute
-
-# def test_get_attribute_not_exist(personality):
-#   assert personality.get_attribute('notfrog') is None
 
 def test_find_personality_by_id(personality):
   pers = find_personality_by_id(personality.personality_id, 0)
